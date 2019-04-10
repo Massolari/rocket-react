1f2f979df930a3deafb858c4662f4f82b0a33c8c
Douglas M <douglasmassolari@nasajon.com.br>
2019-04-10 17:35:53 -0300

    Adicionado mongoose

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
+const port = 8000;
 
 const app = express();
+mongoose.connect('mongodb+srv://massolari:massolari@rocket-react-ecgmd.mongodb.net/omnistack?retryWrites=true', {
+    useNewUrlParser: true
+});
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 console.log('Starting server...');
-app.listen(8000);
+app.listen(port);
+console.log('Server started!');
+console.log(`Listening port ${port}...`);
 
